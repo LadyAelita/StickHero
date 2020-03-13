a80f1272f39b4d175133e48ee187cca737110de8
Powikos <powikosplay@gmail.com>
2020-03-13 19:21:45 +0100

    Main Menu Scripts Completed
    Added: Working mute button, audio folder with bird sounds and few objects.

---
+++ Assets/Scripts/SoundScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundScript : MonoBehaviour
+{
+
+    private Music music;
+    public Button musicToggleButton;
+   // public Sprite musicOnSprite;
+   // public Sprite musicOffSprite;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        music = GameObject.FindObjectOfType<Music>();
+        UpdateIconAndVolume();
+
+    }
+
+  public void PauseMusic()
+    {
+        music.ToogleSound();
+        UpdateIconAndVolume();
+    }
+
+    void UpdateIconAndVolume()
+    {
+        if(PlayerPrefs.GetInt("Muted", 0) == 0)
+        {
+            AudioListener.volume = 1;
+            Debug.Log("Audio On");
+
+        }
+        else
+        {
+            AudioListener.volume = 0;
+            Debug.Log("Audio Off");
+        }
+
+    }
+}
