11611c8334beb0b6a108fa9f03ef507fb4b461d7
Unelith <unelithally333@gmail.com>
2020-03-16 01:33:39 +0100

    Add CameraControl script

---
+++ Assets/Scripts/Game/CameraControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public AnimationControl animControl;
+
+    private Camera cam;
+    private float cameraHalfWidth;
+    private Vector3 cameraStartingLocation;
+
+    public void MoveCameraToStart()
+    {
+        transform.position = cameraStartingLocation;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        cameraHalfWidth = cam.orthographicSize * cam.aspect;
+
+        // Store the starting location
+        cameraStartingLocation = transform.position + new Vector3(cameraHalfWidth, 0.0f);
+        MoveCameraToStart();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
