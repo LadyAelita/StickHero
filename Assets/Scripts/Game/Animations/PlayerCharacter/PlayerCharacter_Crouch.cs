84c3f5fbe334f632760945b7a94828a477d0a4bd
Unelith <unelithally333@gmail.com>
2020-03-16 01:30:11 +0100

    Add PlayerCharacter walk & crouch animations

---
+++ Assets/Scripts/Game/Animations/PlayerCharacter/PlayerCharacter_Crouch.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter_Crouch : StateMachineBehaviour
+{
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // Set 'crouch' flag back to false, so that we only crouch once
+        animator.SetBool("crouch", false);
+    }
+}
