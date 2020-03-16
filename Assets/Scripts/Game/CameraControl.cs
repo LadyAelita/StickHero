77cb1a03854fe8698512d7892201c3f429a10cf4
Unelith <unelithally333@gmail.com>
2020-03-16 02:34:03 +0100

    Move CameraControl initialization actions from Start to Awake

--- Assets/Scripts/Game/CameraControl.cs
+++ Assets/Scripts/Game/CameraControl.cs
@@ -10,20 +10,31 @@ public class CameraControl : MonoBehaviour
     private float cameraHalfWidth;
     private Vector3 cameraStartingLocation;
 
+    public float GetCameraRightEdge()
+    {
+        return cam.ViewportToWorldPoint(new Vector3(1.0f, 0.0f)).x;
+    }
+
     public void MoveCameraToStart()
     {
         transform.position = cameraStartingLocation;
     }
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         cam = GetComponent<Camera>();
         cameraHalfWidth = cam.orthographicSize * cam.aspect;
 
         // Store the starting location
         cameraStartingLocation = transform.position + new Vector3(cameraHalfWidth, 0.0f);
+
         MoveCameraToStart();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
     }
 
     // Update is called once per frame
