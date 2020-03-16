641b4c782ae9d6729202e732389d277d14036b7b
Unelith <unelithally333@gmail.com>
2020-03-16 04:23:05 +0100

    Add stick creation to TerrainGenerator and Game scene

--- Assets/Scripts/Game/TerrainGenerator.cs
+++ Assets/Scripts/Game/TerrainGenerator.cs
@@ -8,15 +8,21 @@ public class TerrainGenerator : MonoBehaviour
     public Camera mainCamera;
     public GameObject startingPlatform;
     public GameObject centerMarkBlueprint;
+    public GameObject stickBlueprint;
     public AnimationControl animControl;
     public float minimalPlatformWidth;
     public float maximalPlatformWidth;
     public float minimalPlatformToPlatformSpacing;
     public float minimalPlatformToRightEdgeSpacing;
+    public float stickGrowthSpeed;
 
     // Private fields
     private GameObject lastPlatform;
+    private GameObject lastStick;
     private CameraControl camControl;
+
+    private bool stickGrowing = false;
+    private bool stickInAir = false;
 
     private float GetMinimalLocalX()
     {
@@ -31,6 +37,18 @@ public class TerrainGenerator : MonoBehaviour
         return camControl.GetCameraRightEdge() - minimalPlatformToRightEdgeSpacing;
     }
 
+    private GameObject InstantiateNewStick()
+    {
+        SpriteRenderer lastPlatformSprite = lastPlatform.GetComponent<SpriteRenderer>();
+        float lastPlatformWidth = lastPlatformSprite.bounds.size.x;
+        Vector3 lastPlatformLocalEdge = lastPlatform.transform.localPosition + new Vector3(lastPlatformWidth, 0.0f);
+
+        GameObject stick = Instantiate(stickBlueprint, transform);
+        stick.transform.localPosition = lastPlatformLocalEdge;
+
+        return stick;
+    }
+
     private GameObject InstantiateNewPlatform(float spawnPosLocalX, float scaleX)
     {
         GameObject platform = Instantiate(startingPlatform, transform);
@@ -40,6 +58,35 @@ public class TerrainGenerator : MonoBehaviour
         platform.transform.localScale = new Vector3(scaleX, platformLocalScale.y, platformLocalScale.z);
 
         return platform;
+    }
+
+    public void SpawnStick()
+    {
+        GameObject stick = InstantiateNewStick();
+        lastStick = stick;
+        stickGrowing = true;
+        stickInAir = true;
+    }
+
+    private void _StickHasFallenCallback(Transform stickTransform)
+    {
+        stickGrowing = false;
+        stickInAir = false;
+        lastStick = null;
+    }
+
+    public void StickFall()
+    {
+        if (lastStick == null) return;
+
+        stickGrowing = false;
+        Vector3 newEuler = new Vector3(0.0f, 0.0f, -90.0f);
+        animControl.AnimateTransformEuler(lastStick.transform, lastStick.transform.eulerAngles, newEuler, _StickHasFallenCallback);
+    }
+
+    public bool IsStickInAir()
+    {
+        return stickInAir;
     }
 
     public void SpawnAndAnimateNextPlatform()
@@ -66,12 +113,23 @@ public class TerrainGenerator : MonoBehaviour
         lastPlatform = startingPlatform;
         camControl = mainCamera.GetComponent<CameraControl>();
 
-        SpawnAndAnimateNextPlatform();
+        //SpawnAndAnimateNextPlatform();
+
+        SpawnStick();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (stickGrowing)
+        {
+            lastStick.transform.localScale += new Vector3(0.0f, stickGrowthSpeed * Time.deltaTime);
+
+            // Stop stick growth roughly after it's length exceeds the viewport's width
+            if (lastStick.transform.localScale.y >= camControl.cameraWidth)
+            {
+                stickGrowing = false;
+            }
+        }
     }
 }
